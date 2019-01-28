767d37e6d67f966b4cfd1ca2effa5f5202574f57
kevinchheang <kevin.chh89@gmail.com>
2019-01-27 23:50:15 -0800

    Implemented registration page

---
+++ src/pages/register/register.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { UserProvider } from '../../providers/user/user'
+
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html'
+})
+export class RegisterPage {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+
+  user = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public _api: UserProvider) {
+    
+  }
+
+  onRegisterUser() {
+    this._api.registerUser(this.user)
+    .subscribe((res: any) => {
+      console.log("Register successful")
+      console.log(res);
+      window.sessionStorage.setItem('token', res.token);
+      window.sessionStorage.setItem('userId', res.userId);
+    }, (err) => {
+      console.log("Error ocurred!");
+    });
+  }
+}
---
+++ src/providers/user/user.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UserProvider {
+  baseUrl: string = "http://localhost:3000/api/appUsers/";
+
+  constructor(public http: HttpClient) {}
+
+  registerUser(user) {
+    return this.http.post(this.baseUrl, user);
+  }
+}
